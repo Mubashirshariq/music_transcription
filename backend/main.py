6ba37449393226e13e3b72ee88a9d463e9286643
Mubashir  Shariq <mubashirsharik01.com>
2024-08-18 04:19:12 +0530

    unique file names to both recording and corresponding mid files

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from fastapi.middleware.cors import CORSMiddleware
 from basic_pitch.inference import predict
 from basic_pitch import ICASSP_2022_MODEL_PATH
+import time
 
 app = FastAPI()
 
@@ -13,7 +14,7 @@ RECORDINGS_DIR = Path('recordings')
 RECORDINGS_DIR.mkdir(parents=True, exist_ok=True)
 
 MIDI_OUTPUT_DIRECTORY = Path('midi')
-MIDI_OUTPUT_DIRECTORY.mkdir(parents=True, exist_ok=True)  # Ensure the directory exists
+MIDI_OUTPUT_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,11 +28,18 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
+# Function to generate a unique filename with a timestamp
+def generate_unique_base_name(directory: Path, base_filename: str) -> str:
+    timestamp = int(time.time())
+    unique_base_name = f"{timestamp}_{base_filename}"
+    return unique_base_name
+
 # Path for recording
 @app.post("/uploadaudio/")
 async def audioUpload(request: Request, audio: UploadFile = File(...)):
     data = await audio.read()
-    save_to = RECORDINGS_DIR / audio.filename
+    base_filename = generate_unique_base_name(RECORDINGS_DIR, audio.filename)
+    save_to = RECORDINGS_DIR / base_filename
     with open(save_to, "wb") as f:
         f.write(data)
     
@@ -42,8 +50,9 @@ async def audioUpload(request: Request, audio: UploadFile = File(...)):
         maximum_frequency=2000.0
     )
     
-    # Save the MIDI data to a file
-    output_midi_path = MIDI_OUTPUT_DIRECTORY / "output.mid"
+    # Save the MIDI data to a file with the same base name
+    midi_filename = f"{base_filename.rsplit('.', 1)[0]}.mid"  # Change extension to .mid
+    output_midi_path = MIDI_OUTPUT_DIRECTORY / midi_filename
     midi_data.write(str(output_midi_path))
 
     return {"filename": audio.filename, "output_midi": str(output_midi_path)}
@@ -52,18 +61,21 @@ async def audioUpload(request: Request, audio: UploadFile = File(...)):
 @app.post("/uploadfile/")
 async def fileUpload(request: Request, file_upload: UploadFile):
     data = await file_upload.read()
-    save_to = UPLOAD_DIR / file_upload.filename
+    base_filename = generate_unique_base_name(UPLOAD_DIR, file_upload.filename)
+    save_to = UPLOAD_DIR / base_filename
     with open(save_to, "wb") as f:
         f.write(data)
 
-     # Run basic_pitch prediction
+    # Run basic_pitch prediction
     model_output, midi_data, note_events = predict(
         str(save_to),
         minimum_frequency=50.0,
         maximum_frequency=2000.0
     )
     
-    # Save the MIDI data to a file
-    output_midi_path = MIDI_OUTPUT_DIRECTORY / "output.mid"
+    # Save the MIDI data to a file with the same base name
+    midi_filename = f"{base_filename.rsplit('.', 1)[0]}.mid"  # Change extension to .mid
+    output_midi_path = MIDI_OUTPUT_DIRECTORY / midi_filename
     midi_data.write(str(output_midi_path))
-    return {"filename": file_upload.filename}
+
+    return {"filename": file_upload.filename, "output_midi": str(output_midi_path)}
